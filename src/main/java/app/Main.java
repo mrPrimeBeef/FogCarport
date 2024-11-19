38269770fc061ca28da4aa019a0abb59eef99bb8
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-11-19 12:59:32 +0100

    Added USER and PASSWORD text for postgres access

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,8 +7,8 @@ import io.javalin.rendering.template.JavalinThymeleaf;
 
 
 public class Main {
-    private static final String USER = "";
-    private static final String PASSWORD = "";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "";
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
