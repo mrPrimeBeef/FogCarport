eccf71ce2cb386af395f897326786e345c6010b8
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2024-11-20 09:34:50 +0100

    formatting update

--- src/main/java/app/controllers/PlainRoofController.java
+++ src/main/java/app/controllers/PlainRoofController.java
@@ -27,13 +27,6 @@ public class PlainRoofController {
         String mobil = ctx.formParam("telefon");
         String email = ctx.formParam("email");
 
-        System.out.println(carportWidth);
-
-
-
         EmailReceipt emailReceipt = new EmailReceipt(carportWidth, carportLength, trapeztag, shedWidth, shedLength, notes, name, adress, zip, city, mobil, email);
-
-
     }
 }
-
