052da6668bbd060bdc4695f22c7962edf7c38746
Peter Bollhorn <pbollhorn@gmail.com>
2024-11-24 16:36:06 +0100

    Progress in rendering dynamic SVGs

--- src/main/java/app/controllers/DeleteMe.java
+++ src/main/java/app/controllers/DeleteMe.java
@@ -1,8 +1,12 @@
 package app.controllers;
 
-import app.persistence.ConnectionPool;
+import java.util.Locale;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import app.persistence.ConnectionPool;
+import app.services.Svg;
 
 public class DeleteMe {
 
@@ -12,19 +16,18 @@ public class DeleteMe {
 
     private static void showSvg(Context ctx, ConnectionPool connectionPool) {
 
-//        System.out.println(String.format("%f tester %s tester", 100.0, "nej"));
+        Locale.setDefault(new Locale("US"));
 
-        String svgText="<svg width=\"100%\" height=\"auto\" viewBox=\"0 0 7800 6000\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
-                "    <rect width=\"100%\" height=\"100%\" fill=\"white\" />\n" +
-                "    <rect x=\"0\" y=\"0\" width=\"45\" height=\"6000\" stroke=\"black\" fill=\"white\"/>\n" +
-                "    <rect x=\"550\" y=\"0\" width=\"45\" height=\"6000\" stroke=\"black\" fill=\"white\"/>\n" +
-                "    <rect x=\"1100\" y=\"0\" width=\"45\" height=\"6000\" stroke=\"black\" fill=\"white\"/>\n" +
-                "    <rect x=\"1650\" y=\"0\" width=\"45\" height=\"6000\" stroke=\"black\" fill=\"white\"/>\n" +
-                "    <rect x=\"2200\" y=\"0\" width=\"45\" height=\"6000\" stroke=\"black\" fill=\"white\"/>\n" +
-                "    <rect x=\"2750\" y=\"0\" width=\"45\" height=\"6000\" stroke=\"black\" fill=\"white\"/>\n" +
-                "    <rect x=\"0\" y=\"100\" width=\"6000\" height=\"45\" stroke=\"black\" fill=\"white\"/>\n" +
-                "</svg>";
-        ctx.attribute("drawing",svgText);
+        Svg carportSvg = new Svg(0, 0, "0 0 855 690", "100%", "auto");
+
+
+        for (int y = 10; y <= 600; y += 50) {
+            carportSvg.addRectangle(10, y, 600, 10, "stroke-width:1px; stroke:#000000; fill: #ffffff");
+        }
+
+        carportSvg.addLine(100,100,200,400, "");
+
+        ctx.attribute("drawing", carportSvg.toString());
         ctx.render("show_svg.html");
     }
 
--- src/main/java/app/services/Svg.java
+++ src/main/java/app/services/Svg.java
@@ -2,36 +2,53 @@ package app.services;
 
 public class Svg {
 
-    private static final String SVG_TEMPLATE = "";
+    private static final String SVG_TEMPLATE = "<svg version=\"1.1\"\n" +
+            "x=\"%d\" y=\"%d\"\n" +
+            "viewBox=\"%s\" width=\"%s\" \n" +
+            "height=\"%s\" preserveAspectRatio=\"xMinYMin\">";
+
+    private static final String SVG_ARROW_DEFS = "";
+
+    private static final String SVG_RECT_TEMPLATE = "<rect x=\"%d\" y=\"%d\" width=\"%f\" height=\"%f\" style=\"%s\"/>";
+
+    private static final String SVG_LINE_TEMPLATE = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\"/>";
+
     private StringBuilder svg = new StringBuilder();
 
-    public Svg(String viewBox, String width, String height) {
-        svg.append("");
-//        String.format("%f tester %s tester", 100.0, "nej");
+//    public Svg(String viewBox, String width, String height) {
+//        svg.append("");
 
-
-    }
-
+    /// /        String.format("%f tester %s tester", 100.0, "nej");
+//
+//
+//    }
     public Svg(int x, int y, String viewBox, String width, String height) {
+        svg.append(String.format(SVG_TEMPLATE, x, y, viewBox, width, height));
+        svg.append(SVG_ARROW_DEFS);
     }
 
-    public void addRectangle(int x, int y, double height, double width, String style) {
+    public void addRectangle(int x, int y, double width, double height, String style) {
+        svg.append(String.format(SVG_RECT_TEMPLATE, x, y, width, height, style));
     }
 
     public void addLine(int x1, int y1, int x2, int y2, String style) {
+        svg.append(String.format(SVG_LINE_TEMPLATE, x1, y1, x2, y2, style));
     }
 
     public void addArrow(int x1, int y1, int x2, int y2, String style) {
+        // Kald addLine med en style der indeholder pilehoveder
     }
 
     public void addText(int x, int y, int rotation, String text) {
     }
 
     public void addSvg(Svg innerSvg) {
+        svg.append(innerSvg.toString());
     }
 
     public String toString() {
-        return null;
+
+        return svg.append("</svg>").toString();
     }
 
 }
