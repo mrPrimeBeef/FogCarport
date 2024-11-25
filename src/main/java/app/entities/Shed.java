724db57dd6271e531354d56f2097ecf0a5151e58
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-11-25 12:57:11 +0100

    Initial Shed class

---
+++ src/main/java/app/entities/Shed.java
@@ -0,0 +1,39 @@
+package app.entities;
+
+public class Shed extends Structure{
+
+    int width;
+    int length;
+    int height;
+    private Carport attachedCarport;
+
+    public Shed(int width, int length, int height) {
+        super(new ShedCalculationStrategy());
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+
+    public void attachToCarport(Carport carport){
+        this.attachedCarport = carport;
+    }
+
+    public Carport getAttachedCarport(){
+        return attachedCarport;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+}
