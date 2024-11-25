6d4d734bbca9af0d75223309ac297b4f49b24799
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-11-25 12:58:46 +0100

    Initial ShedCalculationStrategy Class

---
+++ src/main/java/app/entities/ShedCalculationStrategy.java
@@ -0,0 +1,11 @@
+package app.entities;
+
+import java.util.List;
+
+public class ShedCalculationStrategy implements CalculationStrategy{
+
+    @Override
+    public List<PlacedMaterial> calculateStructure(Structure structure) {
+        return List.of();
+    }
+}
