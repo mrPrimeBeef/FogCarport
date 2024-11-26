831ab687ecddf29772e379a9c4a3f3e0de617e33
Peter Bollhorn <pbollhorn@gmail.com>
2024-11-26 14:05:07 +0100

    Created DetailOrderAccountDto

---
+++ src/main/java/app/dto/DetailOrderAccountDto.java
@@ -0,0 +1,64 @@
+package app.dto;
+
+import java.util.Date;
+
+public class DetailOrderAccountDto {
+    private int orderId;
+    private int accountId;
+    private String email;
+    private String name;
+    private String phone;
+
+    private int zip;
+    private String city;
+
+
+    private Date datePlaced;
+    private Date datePaid;
+    private Date dateCompleted;
+    private double salesPrice;
+    private String status;
+
+    public DetailOrderAccountDto(int orderId, int accountId, String email, Date datePlaced, Date datePaid, Date dateCompleted, double salesPrice, String status) {
+        this.orderId = orderId;
+        this.accountId = accountId;
+        this.email = email;
+        this.datePlaced = datePlaced;
+        this.datePaid = datePaid;
+        this.dateCompleted = dateCompleted;
+        this.salesPrice = salesPrice;
+        this.status = status;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getDatePlaced() {
+        return datePlaced;
+    }
+
+    public Date getDatePaid() {
+        return datePaid;
+    }
+
+    public Date getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
