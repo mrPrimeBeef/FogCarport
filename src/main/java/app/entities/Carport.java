b5de332d097155f2f81612bc088911fda9f7dc07
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-11-26 10:43:37 +0100

    Added comments to explain how to use the carport class. Made changes to how carports are made

--- src/main/java/app/entities/Carport.java
+++ src/main/java/app/entities/Carport.java
@@ -1,23 +1,63 @@
 package app.entities;
 
+import app.persistence.ConnectionPool;
+
 public class Carport extends Structure{
 
-    int width;
-    int length;
-    int height;
+    private int width;
+    private int length;
+    private int height;
     private Shed shed;
+    boolean hasRaisedRoof;
+    RoofType roofType;
+    int angle;
 
-    public Carport(int width, int length, int height) {
-        super(new CarportCalculationStrategy());
+    // *****Temporary comment for integrating carport into other classes:*****
+    // Only the Carport class needs to be imported.
+    // Carport carport = new Carport(width, length, height, shed, hasRaisedRoof, angle);
+    // Carport carport = new Carport(760, 510, 230, null or shed object, false, 0);
+    // Shed can be NULL if no shed is required (shed not implemented).
+    // hasRaisedRoof = false
+    // angle = 0;
+    //
+    // If we need a shed, we need to declare a shed instance before the
+    // carport instance, and include this shed in the carport instance at carport creation
+    // Shed creation is simple (not yet implemented):
+    // Shed shed = new Shed(210, 180, NULL);
+    // Sheds should automagically adjust height after the carport.
+    // Default/lowest height for carport is 210cm, even if height is set to 0
+    public Carport(int width, int length, int height, Shed shed, boolean hasRaisedRoof, int angle, ConnectionPool connectionPool) {
+        super(new CarportCalculationStrategy(connectionPool));
         this.width = width;
         this.length = length;
-        this.height = Math.max(height, 200);
+        this.shed = shed;
+        this.hasRaisedRoof = hasRaisedRoof;
+        this.angle = angle;
+
+        // Sets height to 210 if initial height is lower, also makes height a multiple of 30 if it is not already
+        this.height = Math.max(height, 210);
+        if(this.height % 30 != 0){
+            this.height = (this.height / 30) * 30 + ((this.height % 30 >= 15) ? 30 : 0);
+        }
+
+        this.length = Math.max(length, 240);
+        if(this.length % 30 != 0){
+            this.length = (this.length / 30) * 30 + ((this.length % 30 >= 15) ? 30 : 0);
+        }
+
+        this.width = Math.max(width, 240);
+        if(this.width % 30 != 0){
+            this.width = (this.width / 30) * 30 + ((this.width % 30 >= 15) ? 30 : 0);
+        }
+
+        // Sets Roof Type based on hasRaisedRoof
+        if(hasRaisedRoof){
+            roofType = RoofType.RAISED;
+        }else{
+            roofType = RoofType.FLAT;
+        }
     }
 
-    public void addShed(Shed shed){
-        this.shed = shed;
-        shed.attachToCarport(this);
-    }
 
     public Shed getShed(){
         return shed;
@@ -25,16 +65,20 @@ public class Carport extends Structure{
 
     @Override
     public int getWidth() {
-        return 0;
+        return this.width;
     }
 
     @Override
     public int getLength() {
-        return 0;
+        return this.length;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return this.height;
+    }
+
+    public void setShedHeight(){
+        shed.setHeight(height);
     }
 }
