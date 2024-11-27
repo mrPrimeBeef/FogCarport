1a63ff4ad77214f0c683a434affe30cc6c05ce16
Agnethechr <agnethechr18@gmail.com>
2024-11-27 12:26:17 +0100

    Created an AccountException to be used if we have a try/catch in a method with anything concerning an account.

---
+++ src/main/java/app/exceptions/AccountException.java
@@ -0,0 +1,22 @@
+package app.exceptions;
+
+public class AccountException extends Exception {
+
+    public AccountException(String userMessage) {
+        super(userMessage);
+        System.out.println("userMessage: " + userMessage);
+    }
+
+    public AccountException(String userMessage, String systemMessage) {
+        super(userMessage);
+        System.out.println("userMessage: " + userMessage);
+        System.out.println("systemMessage: " + systemMessage);
+    }
+
+    public AccountException(String userMessage, String developerMessage, String systemMessage) {
+        super(userMessage);
+        System.out.println("userMessage: " + userMessage);
+        System.out.println("developerMessage: " + developerMessage);
+        System.out.println("systemMessage: " + systemMessage);
+    }
+}
